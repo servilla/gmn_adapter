41da2608e2e8d0655b37a14a8e1c5d80dd3e5413
Mark Servilla <mark.servilla@gmail.com>
2025-12-16 18:15:33 -0700

    Summary: Add adapter DB test stub.

---
+++ tests/test_adapter_db.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Test module for the GMN Adapter adapter database model.
+
+Module:
+    test_adapter_db
+
+Author:
+    servilla
+
+Created:
+    2025-12-16
+"""
+import pytest
