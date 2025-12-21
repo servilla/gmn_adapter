a9728218c4bf186818d2cfc89e765535d263bc16
Mark Servilla <mark.servilla@gmail.com>
2025-12-21 14:36:22 -0700

    Summary: Add queue_manager fixture.

--- tests/conftest.py
+++ tests/conftest.py
@@ -12,3 +12,11 @@ Author:
 Created:
     2025-12-16
 """
+import pytest
+
+from gmn_adapter.db.adapter_db import QueueManager
+
+
+@pytest.fixture(scope="session")
+def queue_manager():
+    return QueueManager("./data/adapter_queue.sqlite")
