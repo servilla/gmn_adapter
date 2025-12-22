c7767a519d4035b090f88b4c380111d692a4efea
Mark Servilla <mark.servilla@gmail.com>
2025-12-21 17:32:17 -0700

    Summary: Update queue_manager fixture to load to sqlite memory DB from csv Context: Use static data csv to create sqlite memory-based DB for test suite Change: load csv data into queue_manager (FAILING) and yield queue_manager

--- tests/conftest.py
+++ tests/conftest.py
@@ -12,11 +12,24 @@ Author:
 Created:
     2025-12-16
 """
+import csv
 import pytest
+from sqlalchemy import insert
 
-from gmn_adapter.db.adapter_db import QueueManager
+from gmn_adapter.config import Config
+from gmn_adapter.db.adapter_db import Queue, QueueManager
 
 
 @pytest.fixture(scope="session")
 def queue_manager():
-    return QueueManager("./data/adapter_queue.sqlite")
+    data = Config.ROOT_DIR / "tests" / "data" / "adapter_queue.csv"
+    with open(data, "r") as f:
+        reader = csv.DictReader(f)
+        data = [row for row in reader]
+
+    qm = QueueManager(":memory:")
+    stmt = insert(Queue).values(data)
+    qm.session.execute(stmt)
+    qm.session.commit()
+
+    yield qm
