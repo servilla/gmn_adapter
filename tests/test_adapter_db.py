083835e828c73da67671b7e277c161cbc8e0dd7c
Mark Servilla <mark.servilla@gmail.com>
2025-12-25 12:44:48 -0700

    Summary: Add test_delete_queue. Context: Needed to test the Queue.delete_queue method. Change: Add test_delete_queue, coupled with db memory to file backup.

--- tests/test_adapter_db.py
+++ tests/test_adapter_db.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-"""Test module for the GMN Adapter adapter database model.
+"""Test module for the GMN Adapter adapter database model, and specifically
+the QueueManager class.
 
 Module:
     test_adapter_db
@@ -14,16 +15,36 @@ Created:
 """
 
 import daiquiri
-import pytest
+
+from gmn_adapter.config import Config
+from gmn_adapter.db.adapter_db import QueueManager
+
+import utils.sqlite_utils as su
 
 
 logger = daiquiri.getLogger(__name__)
 
 
 def test_new_queue_manager(queue_manager):
+    """Test that the QueueManager class can be instantiated."""
     assert queue_manager is not None
 
 
-def test_queue_manager_count(queue_manager):
+def test_count(queue_manager):
+    """Test that the queue count is correct."""
     count = queue_manager.get_count()
     assert count == 530
+
+
+def test_delete_queue(queue_manager):
+    """Test that the SQLite database file can be deleted.
+
+    Because the QueueManager class uses an in-memory SQLite database by default,
+    we need to create a file-based database for testing purposes. We then delete
+    the file-based database and verify that it no longer exists.
+    """
+    file_db = Config.ROOT_DIR / "tests" / "data" / "adapter_queue.sqlite"
+    su.sqlite_memory_to_file(queue_manager.engine, str(file_db))
+    qm = QueueManager(str(file_db))
+    qm.delete_queue()
+    assert not file_db.exists()
